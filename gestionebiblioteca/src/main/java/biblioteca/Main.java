53eb2f6e1c3e0bbae5a241373908c75f1ea8c062
Gerardo <gerardo.carino@icloud.com>
2025-11-19 18:34:00 +0100

    Versione 0. Setting del pom.xml e prima stampa del Main

---
+++ gestionebiblioteca/src/main/java/biblioteca/Main.java
@@ -0,0 +1,10 @@
+package biblioteca;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Prima stampa Progetto Ingegneria del Software");
+    }
+}
