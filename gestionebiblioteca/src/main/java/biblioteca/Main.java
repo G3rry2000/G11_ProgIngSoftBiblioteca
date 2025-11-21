d75b1fb7e68efe0582bcb3715c7af9821393f4b1
Gerardo <gerardo.carino@icloud.com>
2025-11-21 16:24:04 +0100

    Versione 1. Aggiunta di Interfaccia Grafica Standard nel main (JavaFX)

--- gestionebiblioteca/src/main/java/biblioteca/Main.java
+++ gestionebiblioteca/src/main/java/biblioteca/Main.java
@@ -1,10 +1,31 @@
 package biblioteca;
 
-/**
- * Hello world!
- */
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button("Clicca qui");
+
+        btn.setOnAction(e -> {
+            System.out.println("Hello World");
+        });
+
+        StackPane root = new StackPane(btn);
+        Scene scene = new Scene(root, 300, 200);
+
+        primaryStage.setTitle("Test JavaFX 8");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Prima stampa Progetto Ingegneria del Software");
+        launch(args);
     }
 }
+
